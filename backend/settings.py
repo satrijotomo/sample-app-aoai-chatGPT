7b3be4df5052ce4b777c9a53331a68eace3c7c5f
Bambang Satrijotomo <bambang.satrijotomo@gmail.com>
2024-08-29 22:42:03 -0500

    Update settings.py

--- backend/settings.py
+++ backend/settings.py
@@ -41,11 +41,11 @@ class _UiSettings(BaseSettings):
         env_ignore_empty=True
     )
 
-    title: str = "Contoso"
+    title: str = "Bambang"
     logo: Optional[str] = None
     chat_logo: Optional[str] = None
     chat_title: str = "Start chatting with Bambang"
-    chat_description: str = "This chatbot is configured to answer your questions"
+    chat_description: str = "This chatbot is configured to answer your questions...if it knows the answer :)"
     favicon: str = "/favicon.ico"
     show_share_button: bool = True
     show_chat_history_button: bool = True
